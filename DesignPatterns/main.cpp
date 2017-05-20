f3cc9769aa19a4b96f6c69527de97b910aea828b
Magiczne <michalkamilkleszczynski@gmail.com>
2017-05-20 09:32:56 +0200

    Created todo list

--- DesignPatterns/main.cpp
+++ DesignPatterns/main.cpp
@@ -1,3 +1,28 @@
+/*
+ * TODO: Prototype
+ * TODO: Builder
+ * TODO: Factory method
+ * TODO: Abstract factory
+ * 
+ * TODO: Adapter
+ * TODO: Decorator
+ * TODO: Facade
+ * TODO: Composite
+ * TODO: Bridge
+ * TODO: Proxy
+ * TODO: Flyweight
+ * 
+ * TODO: CoR
+ * TODO: Command
+ * TODO: Mediator
+ * TODO: Memento
+ * TODO: Observer
+ * TODO: State
+ * TODO: Strategy
+ * TODO: Visitor
+ *
+ */
+
 int main()
 {
 	return 0;
